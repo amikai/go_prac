ac92c906fc42eb442fda0833867c53a78343f00e
amikai-chuang <amikai_chuang@trendmicro.com>
2023-05-01 02:01:56 +0800

    test http handler end to end

--- httpex/http_test.go
+++ httpex/http_test.go
@@ -1,6 +1,7 @@
 package httpex
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -33,4 +34,23 @@ func TestHttpHandler(t *testing.T) {
 	assert.Equal(t, body, []byte(bodyContent))
 }
 
-// TODO: httptest.Server
+func TestHttpHandlerEndToEnd(t *testing.T) {
+	var uppercaseHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		assert.NoError(t, err)
+
+		_, err = w.Write(bytes.ToUpper(body))
+		assert.NoError(t, err)
+	}
+	ts := httptest.NewServer(uppercaseHandler)
+	defer ts.Close()
+	client := ts.Client()
+
+	resp, err := client.Post(ts.URL+"/upper", "application/text", bytes.NewReader([]byte("abc123XYZ")))
+	assert.NoError(t, err)
+
+	respBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "ABC123XYZ", string(respBody))
+}
