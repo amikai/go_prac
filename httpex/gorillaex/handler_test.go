95e99d7dbe91486cb275c32f38c3c6ec6e30d251
amikai <as23041248@gmail.com>
2023-08-08 22:09:07 +0800

    Enhance the gorilla mux comprehensiveness of testing

--- httpex/gorillaex/handler_test.go
+++ httpex/gorillaex/handler_test.go
@@ -25,7 +25,6 @@ func spyGetProductByID(ID string) *db.Product {
 	return nil
 }
 
-// TODO: teet the other product ID that doesn't get the result
 func TestProductHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", fmt.Sprintf("/products/%s", fakeProduct.ID), nil)
 	rr := httptest.NewRecorder()
@@ -40,18 +39,78 @@ func TestProductHandler(t *testing.T) {
 	assert.NoError(t, err)
 
 	wantBody := `
-		{
-		  "data": {
-			"id": "ID-FAKE",
-			"name": "FAKE_NAME"
-		  }
-		}`
+{
+  "data": {
+    "id": "ID-FAKE",
+    "name": "FAKE_NAME"
+  }
+}`
 	assert.Equal(t, resp.StatusCode, http.StatusOK)
 	assert.JSONEq(t, string(wantBody), string(gotBody))
 }
 
+func TestProductNotFound(t *testing.T) {
+	dummyID := "dummy"
+	req := httptest.NewRequest("GET", fmt.Sprintf("/products/%s", dummyID), nil)
+	rr := httptest.NewRecorder()
+	db.GetProductByID = spyGetProductByID
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{id}", ProductHandler).Methods("GET")
+	router.ServeHTTP(rr, req)
+
+	resp := rr.Result()
+	gotBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	wantBody := `{}`
+	assert.JSONEq(t, string(wantBody), string(gotBody))
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+}
+
+var fakeBooks = map[string]*db.Book{
+	"BOOK-999": {
+		ID:       "BOOK-999",
+		Name:     "Fake name",
+		Category: "Fake category",
+	},
+}
+
+var booksByCategory = map[string][]*db.Book{
+	"Fake category": {
+		fakeBooks["BOOK-999"],
+	},
+}
+
+func spyBooksByCategory(category string) []*db.Book {
+	return booksByCategory[category]
+}
+
 func TestBooksCategoryHandler(t *testing.T) {
-	t.Skip("TODO: implement this test")
+	req := httptest.NewRequest("GET", "/books/Fake%20category/", nil)
+	rr := httptest.NewRecorder()
+	db.GetBooksByCategory = spyBooksByCategory
+
+	router := mux.NewRouter()
+	router.HandleFunc("/books/{category}/", BooksCategoryHandler).Methods("GET")
+	router.ServeHTTP(rr, req)
+
+	resp := rr.Result()
+	gotBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	wantBody := `
+{
+  "data": [
+    {
+      "id": "BOOK-999",
+      "name": "Fake name",
+      "category": "Fake category"
+    }
+  ]
+}`
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.JSONEq(t, string(wantBody), string(gotBody))
 }
 
 func TestBooksHandler(t *testing.T) {
