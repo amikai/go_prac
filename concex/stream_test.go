1650c476e8a3d1d569c3016cb2f787b8cbe4a71a
amikai <as23041248@gmail.com>
2023-08-13 03:02:11 +0800

    rewrite conc example, stream is sequential, no need to channel

--- concex/stream_test.go
+++ concex/stream_test.go
@@ -9,28 +9,19 @@ import (
 )
 
 func TestStream(t *testing.T) {
-	out := make(chan string)
-
 	// Stream is used to execute a stream of tasks concurrently while maintaining the order of the results.
 	// Callbacks are called in the same order that tasks are submitted.
 	s := stream.New().WithMaxGoroutines(runtime.NumCPU())
-	go func() {
-		for _, url := range searchURLs {
-			url := url
-			s.Go(func() stream.Callback {
-				result, _ := fakeSearch(url)
-				return func() { out <- result }
-			})
-		}
-		// Wait will not return until all tasks and callbacks have been run.
-		s.Wait()
-		close(out)
-	}()
-
-	results := []string{}
-	for o := range out {
-		results = append(results, o)
+	var got []string
+	for _, url := range searchURLs {
+		url := url
+		s.Go(func() stream.Callback {
+			result, _ := fakeSearch(url)
+			return func() { got = append(got, result) }
+		})
 	}
-	assert.Equal(t, expSearchResult, results)
+	// Wait will not return until all tasks and callbacks have been run.
+	s.Wait()
 
+	assert.Equal(t, expSearchResult, got)
 }
