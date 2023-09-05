bf96cc5c8f080dd154d650dc270bc716223cc68a
amikai <as23041248@gmail.com>
2023-09-05 21:57:56 +0800

    add ff example

--- flagex/ffex/ff_test.go
+++ flagex/ffex/ff_test.go
@@ -1,3 +1,78 @@
 package ffex
 
-// TODO: add example for https://github.com/peterbourgon/ff
+import (
+	"flag"
+	"testing"
+	"time"
+
+	"github.com/peterbourgon/ff"
+	"github.com/peterbourgon/ff/v4/ffyaml"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFFlag(t *testing.T) {
+	args := []string{"-intflag", "12", "-stringflag", "test", "-boolflag", "-durationflag", "10s"}
+
+	var intflag int
+	var boolflag bool
+	var stringflag string
+	var durationflag time.Duration
+
+	fs := flag.NewFlagSet("my_program", flag.ContinueOnError)
+	fs.IntVar(&intflag, "intflag", 0, "int flag value")
+	fs.BoolVar(&boolflag, "boolflag", false, "bool flag value")
+	fs.StringVar(&stringflag, "stringflag", "default", "string flag value")
+	fs.DurationVar(&durationflag, "durationflag", 0, "duration flag value")
+	err := ff.Parse(fs, args)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 12, intflag)
+	assert.True(t, boolflag)
+	assert.Equal(t, "test", stringflag)
+	assert.Equal(t, 10*time.Second, durationflag)
+}
+
+func TestFFlagWithEnvPrefix(t *testing.T) {
+
+	t.Setenv("MY_PROGRAM_INTFLAG", "12")
+	t.Setenv("MY_PROGRAM_BOOLFLAG", "true")
+	t.Setenv("MY_PROGRAM_STRINGFLAG", "test")
+	t.Setenv("MY_PROGRAM_DURATIONFLAG", "10s")
+
+	fs := flag.NewFlagSet("my_program", flag.ContinueOnError)
+	intflag := fs.Int("intflag", 0, "int flag value")
+	boolflag := fs.Bool("boolflag", false, "bool flag value")
+	stringflag := fs.String("stringflag", "default", "string flag value")
+	durationflag := fs.Duration("durationflag", 0, "duration flag value")
+	err := ff.Parse(fs, []string{}, ff.WithEnvVarPrefix("MY_PROGRAM"))
+
+	assert.NoError(t, err)
+	assert.Equal(t, 12, *intflag)
+	assert.True(t, *boolflag)
+	assert.Equal(t, "test", *stringflag)
+	assert.Equal(t, 10*time.Second, *durationflag)
+}
+
+func TestFFlagWithEnvConfig(t *testing.T) {
+	fs := flag.NewFlagSet("my_program", flag.ContinueOnError)
+	intflag := fs.Int("intflag", 0, "int flag value")
+	boolflag := fs.Bool("boolflag", false, "bool flag value")
+	stringflag := fs.String("stringflag", "default", "string flag value")
+	durationflag := fs.Duration("durationflag", 0, "duration flag value")
+	_ = fs.String("config", "", "config file")
+	err := ff.Parse(fs, []string{"-config", "testdata/config.yaml"}, ff.WithConfigFileFlag("config"), ff.WithConfigFileParser(ffyaml.Parse))
+
+	assert.NoError(t, err)
+	assert.Equal(t, 12, *intflag)
+	assert.True(t, *boolflag)
+	assert.Equal(t, "test", *stringflag)
+	assert.Equal(t, 10*time.Second, *durationflag)
+}
+
+func TestFFLognOption(t *testing.T) {
+	// TODO: implement the long option
+}
+
+func TestFFMixOptionEnvConf(t *testing.T) {
+	// TODO: implement mixing options, env variable and config file
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/gwatts/gin-adapter v1.0.0
 	github.com/lossdev/stack v1.0.5
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/peterbourgon/ff v1.7.1
+	github.com/peterbourgon/ff/v4 v4.0.0-alpha.1
 	github.com/sourcegraph/conc v0.3.0
 	github.com/spf13/viper v1.13.0
 	github.com/stretchr/testify v1.8.4
