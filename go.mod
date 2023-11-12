68b2d29fa078e7cc4e20f2ff75926fe6ae90930a
amikai <as23041248@gmail.com>
2023-11-13 00:00:33 +0800

    add redigo get set example

--- go.mod
+++ go.mod
@@ -38,6 +38,7 @@ require (
 	github.com/go-playground/validator/v10 v10.15.3 // indirect
 	github.com/go-redis/redismock/v9 v9.2.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
---
+++ redisex/redigoex/keyval_test.go
@@ -0,0 +1,50 @@
+package goredisex
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/gomodule/redigo/redis"
+	"github.com/stretchr/testify/assert"
+)
+
+func newPool(address string) *redis.Pool {
+	return &redis.Pool{
+		// Maximum number of idle connections in the pool.
+		MaxIdle: 80,
+		// max number of connections
+		MaxActive: 12000,
+		// Dial is an application supplied function for creating and
+		// configuring a connection.
+		DialContext: func(ctx context.Context) (redis.Conn, error) {
+			c, err := redis.DialContext(ctx, "tcp", address)
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
+}
+
+func TestGetSet(t *testing.T) {
+	s := miniredis.RunT(t)
+	pool := newPool(s.Addr())
+	conn := pool.Get()
+	defer conn.Close()
+
+	resp, err := redis.DoContext(conn, context.Background(), "PING")
+	resp, err = redis.String(resp, err)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, "PONG")
+
+	resp, err = redis.DoContext(conn, context.Background(), "SET", "key", "value")
+	resp, err = redis.String(resp, err)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, "OK")
+
+	resp, err = redis.DoContext(conn, context.Background(), "GET", "key")
+	resp, err = redis.String(resp, err)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, "value")
+}
