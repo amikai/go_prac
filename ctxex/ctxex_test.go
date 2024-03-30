b89a069dfb4fddcb2c2e3ca54ccd8673b1971d4e
amikai <as23041248@gmail.com>
2024-03-30 08:57:54 +0800

    add more example for context usage

--- ctxex/ctxex_test.go
+++ ctxex/ctxex_test.go
@@ -50,3 +50,67 @@ func TestCancelCause(t *testing.T) {
 	assert.ErrorIs(t, ctx.Err(), context.Canceled)
 	assert.ErrorIs(t, context.Cause(ctx), causeErr)
 }
+
+func TestDeadlineCancelCause(t *testing.T) {
+	t.Run("cancel after timeout", func(t *testing.T) {
+		causeErr := errors.New("cause")
+		ctx, cancel := context.WithTimeoutCause(context.Background(), 0, causeErr)
+		defer cancel()
+
+		assert.ErrorIs(t, ctx.Err(), context.DeadlineExceeded)
+		assert.ErrorIs(t, context.Cause(ctx), causeErr)
+		assert.NotErrorIs(t, ctx.Err(), context.Canceled)
+
+	})
+
+	t.Run("cancel before timeout", func(t *testing.T) {
+		causeErr := errors.New("cause")
+		ctx, cancel := context.WithTimeoutCause(context.Background(), 1*time.Second, causeErr)
+		cancel()
+
+		assert.ErrorIs(t, ctx.Err(), context.Canceled)
+		assert.NotErrorIs(t, ctx.Err(), context.DeadlineExceeded)
+		assert.NotErrorIs(t, context.Cause(ctx), causeErr)
+
+		time.Sleep(1500 * time.Millisecond)
+
+		assert.ErrorIs(t, ctx.Err(), context.Canceled)
+		assert.NotErrorIs(t, ctx.Err(), context.DeadlineExceeded)
+		assert.NotErrorIs(t, context.Cause(ctx), causeErr)
+	})
+
+	t.Run("verfiy child or parent context I", func(t *testing.T) {
+		parentCauseErr := errors.New("parent cause")
+		parentCtx, cancel := context.WithTimeoutCause(context.Background(), 1*time.Second, parentCauseErr)
+		defer cancel()
+
+		childCauseErr := errors.New("child cause")
+		childCtx, cancel := context.WithTimeoutCause(parentCtx, 0, childCauseErr)
+		defer cancel()
+
+		assert.NoError(t, parentCtx.Err())
+		assert.NoError(t, context.Cause(parentCtx))
+
+		assert.ErrorIs(t, childCtx.Err(), context.DeadlineExceeded)
+		assert.ErrorIs(t, context.Cause(childCtx), childCauseErr)
+	})
+
+	t.Run("verfiy child or parent context II", func(t *testing.T) {
+		parentCauseErr := errors.New("parent cause")
+		parentCtx, cancel := context.WithTimeoutCause(context.Background(), 1*time.Second, parentCauseErr)
+		defer cancel()
+
+		childCauseErr := errors.New("child cause")
+		childCtx, cancel := context.WithTimeoutCause(parentCtx, 0, childCauseErr)
+		defer cancel()
+
+		time.Sleep(1500 * time.Millisecond)
+
+		assert.ErrorIs(t, parentCtx.Err(), context.DeadlineExceeded)
+		assert.ErrorIs(t, context.Cause(parentCtx), parentCauseErr)
+		assert.NotErrorIs(t, context.Cause(parentCtx), childCauseErr)
+
+		assert.ErrorIs(t, childCtx.Err(), context.DeadlineExceeded)
+		assert.ErrorIs(t, context.Cause(childCtx), childCauseErr)
+	})
+}
