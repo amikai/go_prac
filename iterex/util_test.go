7cd3933aea8248ac2f56d0efb4dc8b65410f5cea
amikai <as23041248@gmail.com>
2024-06-16 10:30:19 +0800

    chore: organize the function

---
+++ iterex/util_test.go
@@ -0,0 +1,46 @@
+package iterex
+
+import (
+	"iter"
+	"slices"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// https://github.com/golang/go/issues/61898
+func Filter[V any](f func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range seq {
+			if f(v) && !yield(v) {
+				return
+			}
+		}
+		return
+	}
+}
+
+func TestFilter(t *testing.T) {
+	seq := slices.Values([]int{-1, -2, 0, 1, 2})
+	positiveSeq := Filter(func(v int) bool { return v > 0 }, seq)
+	assert.Equal(t, []int{1, 2}, slices.Collect(positiveSeq))
+}
+
+// https://github.com/golang/go/issues/61898
+func Map[In, Out any](f func(In) Out, seq iter.Seq[In]) iter.Seq[Out] {
+	return func(yield func(Out) bool) {
+		for in := range seq {
+			if !yield(f(in)) {
+				return
+			}
+		}
+		return
+	}
+}
+
+func TestMap(t *testing.T) {
+	seqInt := slices.Values([]int{0, 1, 2})
+	seqStr := Map(func(v int) string { return strconv.Itoa(v) }, seqInt)
+	assert.Equal(t, []string{"0", "1", "2"}, slices.Collect(seqStr))
+}
