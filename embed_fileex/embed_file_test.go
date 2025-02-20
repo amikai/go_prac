a75909fc6e1feb18fd3862735d378e9a556582bf
amikai <as23041248@gmail.com>
2025-02-20 23:20:05 +0800

    add embed fs example

--- embed_fileex/embed_file_test.go
+++ embed_fileex/embed_file_test.go
@@ -1,10 +1,11 @@
 package embedfileex
 
 import (
-	_ "embed"
+	"embed"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 //go:embed file.txt
@@ -13,10 +14,23 @@ var s string
 //go:embed file.txt
 var b []byte
 
+//go:embed file.txt amikai.txt
+var fs embed.FS
+
 func TestEmbedString(t *testing.T) {
 	assert.Equal(t, "hello world\n", s)
 }
 
 func TestEmbedBytes(t *testing.T) {
 	assert.Equal(t, []byte("hello world\n"), b)
+}
+
+func TestEmbeddedFs(t *testing.T) {
+	b, err := fs.ReadFile("file.txt")
+	require.NoError(t, err)
+	assert.Equal(t, []byte("hello world\n"), b)
+
+	b, err = fs.ReadFile("amikai.txt")
+	require.NoError(t, err)
+	assert.Equal(t, []byte("Hi, Amikai\n"), b)
 }
