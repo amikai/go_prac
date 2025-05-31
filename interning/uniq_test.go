15cc91be8562ee551ea5fb14aeade7990569f8ba
amikai <as23041248@gmail.com>
2025-06-01 01:01:34 +0800

    feat: add uniq comparsion code

--- interning/uniq_test.go
+++ interning/uniq_test.go
@@ -11,6 +11,9 @@ func TestCompareHandle(t *testing.T) {
 	s1 := unique.Make("abcdefghijklmnoppqrstuvwxyz")
 	s2 := unique.Make("abcdefghijklmnoppqrstuvwxyz")
 	assert.True(t, s1 == s2)
+
+	assert.Equal(t, "abcdefghijklmnoppqrstuvwxyz", s1.Value())
+	assert.Equal(t, "abcdefghijklmnoppqrstuvwxyz", s2.Value())
 }
 
 func BenchCompare(b *testing.B) {
